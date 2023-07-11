a9ca8248603ceda00219646f023e7e2afb3ca892
GonzaloRossin <gonzarossin@gmail.com>
2023-07-11 19:40:29 -0300

    fea: basic file reader done

--- client/react/src/pages/FileReader.tsx
+++ client/react/src/pages/FileReader.tsx
@@ -1,10 +1,30 @@
+import { Container, Typography } from "@mui/material";
 import { FC } from "react";
+import tp4_60135PDF from "../../public/tp4_60135.pdf";
 
 const FileReader: FC = () => {
 
     return(
-        <div>
-            <h1>File reader Page</h1>
+        <div
+        style={{
+            display: "flex",
+            alignItems: "center",
+            flexDirection: "column",
+            height: "90vh",
+            width: "80vw",
+        }}
+        >
+        <div style={{
+            height: "100%",
+            width: 1000,
+        }}>
+            <object
+                data={tp4_60135PDF}
+                type="application/pdf"
+                width="100%"
+                height="98%"
+            />
+        </div>
         </div>
     );
 }
