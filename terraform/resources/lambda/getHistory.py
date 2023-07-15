15b431552a42f421143acb56fc145273f5db460d
Mauro Sambartolomeo <msambartolomeo@itba.edu.ar>
2023-07-15 16:40:34 -0300

    update gethistory lambda

--- terraform/resources/lambda/getHistory.py
+++ terraform/resources/lambda/getHistory.py
@@ -17,7 +17,27 @@ def handler(event, context):
     bucket_name = os.environ.get("BUCKET_NAME")
 
     userId = event["pathParameters"]["userId"]
-    email = base64.b64decode(userId.encode("utf-8")).decode("utf-8")
+    try:
+        email = base64.b64decode(userId.encode("utf-8")).decode("utf-8")
+    except:
+        return {"statusCode": 400, "body": json.dumps("Path parameter must be base64")}
+
+    try:
+        authorizedEmail = event["requestContext"]["authorizer"]["claims"]["email"]
+        logger.info(authorizedEmail)
+        if authorizedEmail != email:
+            # check if it is doctor's email
+            item = dynamo.get_item(
+                TableName="doctors-table",
+                Key={
+                    "pk": {"S": authorizedEmail},
+                    "sk": {"S": userId},
+                },
+            )["Item"]
+            if not item:
+                raise Exception
+    except:
+        return {"statusCode": 403}
 
     try:
         item = dynamo.get_item(
@@ -33,7 +53,7 @@ def handler(event, context):
 
     try:
         url = s3.generate_presigned_url(
-            "get_object", Params={"Bucket": bucket_name, "Key": file_key}, ExpiresIn=100
+            "get_object", Params={"Bucket": bucket_name, "Key": file_key}, ExpiresIn=20
         )
         message = f'File "{file_key}" found in bucket "{bucket_name}"'
         logger.info(message)
@@ -42,7 +62,7 @@ def handler(event, context):
 
     return {
         "statusCode": 200,
-        "body": json.dumps({"url": url, "event": event}),
+        "body": json.dumps({"url": url}),
         "headers": {
             "Content-Type": "application/json",
         },
