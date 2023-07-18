1e82ce99f552c2a0340d5db5ddad4511b9cb876e
Amparo1999 <116674796+Amparo1999@users.noreply.github.com>
2023-07-17 21:01:08 -0300

    Front-Pagina

--- client/react/src/pages/DoctorPage.tsx
+++ client/react/src/pages/DoctorPage.tsx
@@ -1,12 +1,29 @@
-import { FC } from "react";
+import React from 'react';
+import { Box, Button, Grid, Typography, TextField, Paper} from '@mui/material';
 
-const DoctorPage: FC = () => {
-
-    return(
-        <div>
-            <h1>Doctor Page</h1>
-        </div>
-    );
-}
+const DoctorPage: React.FC = () => {
+  return (
+    <Box textAlign="center">
+        <Grid item xs={12}>
+          <Paper>
+          <Typography variant="h2" className="title">
+              Pagina de doctor  
+            </Typography>
+            <Grid item xs={12} style={{ marginBottom: '1rem', marginTop: '1rem'}}>
+              <TextField variant="outlined" className="textfield" label="Nombre de doctor" />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField variant="outlined" className="textfield" label="Nombre de paciente" />
+            </Grid>
+            <Grid style={{ marginTop: '2rem' }}>
+              <Button variant="outlined">Ver hitorial</Button>
+              <Button variant="outlined"  style={{ marginLeft: '2rem'}}>Cargar historial</Button>
+            </Grid>
+            
+          </Paper>
+        </Grid>
+    </Box>
+  );
+};
 
 export default DoctorPage;
